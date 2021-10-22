f1b73904358cf4a54f10ec69bc9fbd025ca79984
hamaduzi <mohammedkarem9898@gmail.com>
2021-10-22 23:40:44 +0300

    sockets added

--- Proxy.java
+++ Proxy.java
@@ -5,10 +5,14 @@ public class Proxy
 {
 	public static void main(String[] args) {
 		try{
-			ServerSocket server = new ServerSocket(3333);
+			ServerSocket server = new ServerSocket(9999);
 			while(true){
 				Socket c2p = server.accept();
-				Socket p2s = new Socket("127.0.0.1", 4444);
+				Socket p2serverA = new Socket("127.0.0.1", 1111);
+				Socket p2serverE = new Socket("127.0.0.1", 2222);
+				Socket p2serverI = new Socket("127.0.0.1", 3333);
+				Socket p2serverO = new Socket("127.0.0.1", 4444);
+				Socket p2serverU = new Socket("127.0.0.1", 5555);
 
 				Thread clientThread = new Thread(new Runnable (){
 						public void run(){
@@ -16,8 +20,8 @@ public class Proxy
 								final DataInputStream c2pIn = new DataInputStream(c2p.getInputStream()) ;
                                 final DataOutputStream c2pOut = new DataOutputStream(c2p.getOutputStream()) ;
                                 
-								final DataInputStream p2sIn = new DataInputStream(p2s.getInputStream()) ;
-								final DataOutputStream p2sOut =new DataOutputStream(p2s.getOutputStream()) ;
+								final DataInputStream p2sIn = new DataInputStream(p2serverA.getInputStream()) ;
+								final DataOutputStream p2sOut =new DataOutputStream(p2serverA.getOutputStream()) ;
 
 								Thread t1 = new Thread(new Runnable(){
 										public void run(){
