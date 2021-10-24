524722662e9e908d354a317622399c65b43c8956
sazan-barznji <barznjisazan99@yahoo.com>
2021-10-25 00:17:29 +0300

    words counting function added to all servers

--- MyClient.java
+++ MyClient.java
@@ -6,11 +6,11 @@ public class MyClient {
     public static void main(String[] args) {
         try {
             Socket s = new Socket("127.0.0.1", 5555);
-            // DataOutputStream dout = new DataOutputStream(s.getOutputStream());
-            // dout.writeUTF("hello this is assignment 1 of distrbuted computing");
-            // dout.flush();           
-            // dout.close();
-            // s.close();
+            DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+            dout.writeUTF("hello this is assignment 1 of distrbuted computing");
+            dout.flush();           
+            dout.close();
+            s.close();
 
         } catch (Exception e) {
             System.out.println(e);
--- ServerO.java
+++ ServerO.java
@@ -16,6 +16,9 @@ public class ServerO {
             DataOutputStream dout= new DataOutputStream (s.getOutputStream());
             dout.writeInt(numberOfVowls);
             
+            int numberOfWords= countWordsInSentence(str);
+            dout.writeInt(numberOfWords);
+
             ss.close();
         } catch (Exception e) {
             System.out.println(e);
@@ -31,5 +34,26 @@ public class ServerO {
 		}
 	
 		return count;
-	}
+    }
+    
+    private static int countWordsInSentence(String input) {
+        int wordCount = 0;
+    
+        if (input.trim().equals("")) {
+            return wordCount;
+        }
+        else {
+            wordCount = 1;
+        }
+    
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            String str = new String("" + ch);
+            if (i+1 != input.length() && str.equals(" ") && !(""+ input.charAt(i+1)).equals(" ")) {
+                wordCount++;
+            }
+        }
+    
+        return wordCount;
+    }
 }
